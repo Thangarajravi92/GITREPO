01293ffc49357939653e28f578ab278ce91624f2
Thangaraj <thangarajravi92@gmail.com>
2024-03-24 15:31:53 +0530

    Second commit

--- GIT_1/src/Project_1_GIT.java
+++ GIT_1/src/Project_1_GIT.java
@@ -4,6 +4,8 @@ public class Project_1_GIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to GIT");
+		System.out.println("GIT REPO");
+		System.out.println("GIT REPOsitory");
 	}
 
 }
