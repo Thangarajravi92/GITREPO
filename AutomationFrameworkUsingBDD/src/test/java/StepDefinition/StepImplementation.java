d10a5aff1575ba1c1fa9a515d55593219b696ecb
Thangaraj <thangarajravi92@gmail.com>
2024-03-24 16:26:48 +0530

    3rd commit

--- AutomationFrameworkUsingBDD/src/test/java/StepDefinition/StepImplementation.java
+++ AutomationFrameworkUsingBDD/src/test/java/StepDefinition/StepImplementation.java
@@ -164,11 +164,11 @@ public class StepImplementation {
 	@Before
 	public void preconditionsteps() throws IOException
 	{
-		FileInputStream config_fis = new FileInputStream("F:\\Eclipse_Workspace\\AutomationFrameworkUsingBDD\\src\\test\\resources\\Configuration\\Config.properties");
+		FileInputStream config_fis = new FileInputStream("C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\MavenCICD\\AutomationFrameworkUsingBDD\\src\\test\\resources\\Configuration\\Config.properties");
 		Configprop = new Properties();
 		Configprop.load(config_fis);
 		
-		FileInputStream Object_fis = new FileInputStream("F:\\Eclipse_Workspace\\AutomationFrameworkUsingBDD\\src\\test\\resources\\Configuration\\ObjectRepo.properties");
+		FileInputStream Object_fis = new FileInputStream("C:\\ProgramData\\Jenkins\\.jenkins\\workspace\\MavenCICD\\AutomationFrameworkUsingBDD\\src\\test\\resources\\Configuration\\ObjectRepo.properties");
 		Objectprop = new Properties();
 		Objectprop.load(Object_fis);
 		
